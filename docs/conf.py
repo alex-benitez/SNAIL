32ae3429a560df398b5eb66a7a850415f2eeabfe
Alex <alexbenitezcanelles@gmail.com>
2025-05-29 14:05:40 +0100

    Padding my commit history

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ extensions = [ 'sphinx.ext.autodoc',
     'sphinx_rtd_theme',]
 
 templates_path = ['_templates']
+html_static_path = ["_static"]
 exclude_patterns = []
 
 
@@ -31,3 +32,4 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_logo = "_static/snailtransparent.png"
